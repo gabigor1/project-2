6a08f808e642074e4f74a887f6c690d26cbce1b9
Gabriel Goiri <gabrielgoiri@gmail.com>
2020-05-01 11:17:25 +0100

    almost done styling the cards and the images

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const RecipeCard = ({ title, image, sourceUrl}) => (
-  <div className="desktop">
     <div className="card">
-      <div className="card-title">
-        <h2>{title}</h2>
-      </div>
       <div className="card-image">
         <a href={sourceUrl}> <img src={image} alt={title}/></a>
       </div>
+      <div className="card-title">
+        <h2>{title}</h2>
+      </div>
     </div>
-  </div>
 )
 
 export default RecipeCard
